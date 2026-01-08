b5d15cc104fc823b0254bfc14ba51675d4b72bdf
antho <megamrd102@gmail.com>
2026-01-08 00:05:34 -0500

    Laptop Formateada y codigo recuperado (08-01-2026)

--- app/src/main/java/com/example/proyectodepspot/data/DepresionDetector.kt
+++ app/src/main/java/com/example/proyectodepspot/data/DepresionDetector.kt
@@ -335,7 +335,7 @@ class DepresionDetector(private val context: Context) {
 
                     // Enviar el correo usando Resend
                     val response = resendService.sendEmail(
-                        apiKey = "zzz",
+                        apiKey = "zzzc",
                         emailRequest = emailRequest
                     )
 
